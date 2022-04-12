a66be9a129829e27ab10a741e01420dab3a4f9fd
jasonhope <jhope766@anselm.edu>
2022-04-11 21:05:58 -0400

    rinkby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x4313A7d0EbFD80517f28Cd8DB75D79b8CA1E5e59"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";

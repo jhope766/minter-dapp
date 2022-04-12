021579d9882be9178a838b7767a2f804f6a34757
jasonhope <jhope766@anselm.edu>
2022-04-11 21:41:34 -0400

    updated colors

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x4313A7d0EbFD80517f28Cd8DB75D79b8CA1E5e59"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the SportApes NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The SportApes NFT Project is a decentralized, open-source project.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
